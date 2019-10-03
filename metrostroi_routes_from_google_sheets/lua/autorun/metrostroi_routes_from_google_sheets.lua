17114ef9289538390319a229da7c05911641adee
TheFulDeep <zlobindd@mail.ru>
2019-10-03 17:59:32 +0300

    customization added

--- metrostroi_routes_from_google_sheets/lua/autorun/metrostroi_routes_from_google_sheets.lua
+++ metrostroi_routes_from_google_sheets/lua/autorun/metrostroi_routes_from_google_sheets.lua
@@ -2,9 +2,173 @@ if CLIENT then
 	local Font = CreateClientConVar("metrostroi_routes_font", "Trebuchet24", true)--ScrW() default
 	local font = Font:GetString()
 
+	local ypos = CreateClientConVar("metrostroi_routes_ypos", 0, true)--ScrH() default
+	local xpos = CreateClientConVar("metrostroi_routes_xpos", 0, true)--ScrW() default
+	local xPos = xpos:GetInt()
+	local yPos = ypos:GetInt()
+
+	local Alph = CreateClientConVar("metrostroi_routes_alph", 30, true)
+	local Alph2 = CreateClientConVar("metrostroi_routes_alph2", 0, true)
+	local Alph3 = CreateClientConVar("metrostroi_routes_alph3", 0, true)
+	local Alph4 = CreateClientConVar("metrostroi_routes_alph4", 0, true)
+	local alph = 255-Alph:GetInt()
+	local alph2 = 255-Alph2:GetInt()
+	local alph3 = 255-Alph3:GetInt()
+	local alph4 = 255-Alph4:GetInt()
+
+	local r1ConVar = CreateClientConVar("metrostroi_routes_r1", 0, true)
+	local r2ConVar = CreateClientConVar("metrostroi_routes_r2", 255, true)
+	local r3ConVar = CreateClientConVar("metrostroi_routes_r3", 255, true)
+	local r4ConVar = CreateClientConVar("metrostroi_routes_r4", 0, true)
+	local g1ConVar = CreateClientConVar("metrostroi_routes_g1", 0, true)
+	local g2ConVar = CreateClientConVar("metrostroi_routes_g2", 255, true)
+	local g3ConVar = CreateClientConVar("metrostroi_routes_g3", 255, true)
+	local g4ConVar = CreateClientConVar("metrostroi_routes_g4", 0, true)
+	local b1ConVar = CreateClientConVar("metrostroi_routes_b1", 0, true)
+	local b2ConVar = CreateClientConVar("metrostroi_routes_b2", 255, true)
+	local b3ConVar = CreateClientConVar("metrostroi_routes_b3", 255, true)
+	local b4ConVar = CreateClientConVar("metrostroi_routes_b4", 0, true)
+
+	local r1 = r1ConVar:GetInt()
+	local r2 = r2ConVar:GetInt()
+	local r3 = r3ConVar:GetInt()
+	local r4 = r4ConVar:GetInt()
+	local g1 = g1ConVar:GetInt()
+	local g2 = g2ConVar:GetInt()
+	local g3 = g3ConVar:GetInt()
+	local g4 = g4ConVar:GetInt()
+	local b1 = b1ConVar:GetInt()
+	local b2 = b2ConVar:GetInt()
+	local b3 = b3ConVar:GetInt()
+	local b4 = b4ConVar:GetInt()
+
+	local SecretConVar = CreateClientConVar("metrostroi_routes_secret", 0, true)
+	local Secret = SecretConVar:GetFloat()
+
 	local RouteList
 	local MaxCols = {}
 	local Cols = {}
+
+	local h1,w1
+	local function GetW(RowNumber,ColNumber)
+		local w = 0
+		for i = 1,ColNumber do 
+			if i == ColNumber then continue end
+			w1,h1 = surface.GetTextSize(MaxCols[i] or "")
+			w = w1 + w + 10
+		end
+		return w
+	end
+
+	local function GetH(RowNumber)
+		local h2 = 0
+		for i = 1, RowNumber do
+			local w,h = surface.GetTextSize("")
+			h2 = h2 + h
+		end
+		return h2
+	end
+
+	local Maxs = {}
+	local Lines = {}
+	local function MathLines()
+		local sdvig = 5
+		Lines = {}
+		for i = 1,#RouteList-1 do
+			Lines[#Lines+1] = {}
+			Lines[#Lines].Start = {}
+			Lines[#Lines].End = {}
+
+			Lines[#Lines].Start.x = xPos+GetW(i,1) - sdvig
+			Lines[#Lines].Start.y = yPos+GetH(i,1)
+
+			Lines[#Lines].End.x = xPos+Maxs[3] -1 -- - sdvig
+			Lines[#Lines].End.y = yPos+GetH(i,#Cols)
+		end
+
+		for i = 2,#Cols do
+			Lines[#Lines+1] = {}
+			Lines[#Lines].Start = {}
+			Lines[#Lines].End = {}
+
+			Lines[#Lines].Start.x = xPos+GetW(2,i) - sdvig
+			if i == 2 then 
+				Lines[#Lines].Start.y = yPos
+			else
+				Lines[#Lines].Start.y = yPos+GetH(2,i)
+			end
+
+			Lines[#Lines].End.x = xPos+GetW(#RouteList,i) - sdvig
+			Lines[#Lines].End.y = yPos+GetH(#RouteList,i)
+		end
+	end
+
+	local StringsPositions = {}
+	local function MathStringsPositions()
+		print("Math")
+		if not RouteList then return end
+		surface.SetFont(font)
+		xPos = xpos:GetInt()
+		yPos = ypos:GetInt()
+		--[[alph = 255-Alph:GetInt()
+		alph2 = 255-Alph2:GetInt()
+		alph3 = 255-Alph3:GetInt()
+		alph4 = 255-Alph4:GetInt()
+		r1 = r1ConVar:GetInt()
+		r2 = r2ConVar:GetInt()
+		r3 = r3ConVar:GetInt()
+		r4 = r4ConVar:GetInt()
+		g1 = g1ConVar:GetInt()
+		g2 = g2ConVar:GetInt()
+		g3 = g3ConVar:GetInt()
+		g4 = g4ConVar:GetInt()
+		b1 = b1ConVar:GetInt()
+		b2 = b2ConVar:GetInt()
+		b3 = b3ConVar:GetInt()
+		b4 = b4ConVar:GetInt()
+
+		Secret = SecretConVar:GetFloat()]]
+		StringsPositions = {}
+
+		Cols = {}
+		for RowNumber,Col in pairs(RouteList) do
+			for k,v in pairs(Col) do
+				if not Cols[k] then Cols[k] = {} end
+				Cols[k][RowNumber] = v
+			end
+		end
+
+		for RowNumber,Col in pairs(RouteList) do
+			for ColNumber,val in pairs(Col) do
+				if val == "-" then continue end
+				if tonumber(val) and val/10 < 1 then val = "0"..val end
+				local XPos = xPos+(GetW(RowNumber,ColNumber) or 0)
+				local YPos = yPos+(h1 and h1*RowNumber or 0)-(h1 or 0)
+				StringsPositions[#StringsPositions+1] = {}
+				StringsPositions[#StringsPositions].val = val
+				StringsPositions[#StringsPositions].xpos = XPos
+				StringsPositions[#StringsPositions].ypos = YPos
+				StringsPositions[#StringsPositions].col = ColNumber
+				StringsPositions[#StringsPositions].row = RowNumber
+				local w,h = surface.GetTextSize(val)
+				StringsPositions[#StringsPositions].w = w
+				StringsPositions[#StringsPositions].h = h
+				StringsPositions[#StringsPositions].wB = math.abs(StringsPositions[#StringsPositions].xpos-xPos)+w
+				StringsPositions[#StringsPositions].hB = math.abs(StringsPositions[#StringsPositions].ypos-yPos)+h
+			end
+		end
+
+		local MaxX,MaxY
+		for _,str in pairs(StringsPositions) do
+			if not MaxX or MaxX < str.wB then MaxX = str.wB end
+			if not MaxY or MaxY < str.hB then MaxY = str.hB end
+		end
+		Maxs = {MaxX,MaxY,MaxX,GetH(#RouteList)}
+
+		MathLines()
+		--PrintTable(StringsPositions)
+	end
+
 	net.Receive( "Metrostroi Routes From Google Sheets", function() 
 		local IsChatMessage = net.ReadBool()
 		local String = net.ReadString()
@@ -39,7 +203,7 @@ if CLIENT then
 			end
 		end
 
-		local Cols = {}
+		Cols = {}
 		for RowNumber,Col in pairs(RouteList) do
 			for k,v in pairs(Col) do
 				if not Cols[k] then Cols[k] = {} end
@@ -58,19 +222,37 @@ if CLIENT then
 			end
 			MaxCols[ColNumber] = MaxStr
 		end
+		MathStringsPositions()
 	end)
-	
-	
-	local ypos = CreateClientConVar("metrostroi_routes_ypos", 0, true)--ScrH() default
-	local xpos = CreateClientConVar("metrostroi_routes_xpos", 0, true)--ScrW() default
 
 	cvars.AddChangeCallback( "metrostroi_routes_font", function(convar,olval,newval)
 		if newval:lower():find("%a") then
 			font = newval
+			MathStringsPositions()
 		else
 			chat.AddText(color_white,"Нельзя указать пустой шрифт")
 		end
 	end)
+
+	cvars.AddChangeCallback( "metrostroi_routes_xpos", function(convar,olval,newval)MathStringsPositions()end)
+	cvars.AddChangeCallback( "metrostroi_routes_ypos", function(convar,olval,newval)MathStringsPositions()end)
+	cvars.AddChangeCallback( "metrostroi_routes_alph", function(convar,olval,newval)alph = 255-newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_alph2", function(convar,olval,newval)alph2 = 255-newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_alph3", function(convar,olval,newval)alph3 = 255-newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_alph4", function(convar,olval,newval)alph4 = 255-newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_r1", function(convar,olval,newval)r1 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_r2", function(convar,olval,newval)r2 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_r3", function(convar,olval,newval)r3 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_r4", function(convar,olval,newval)r4 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_g1", function(convar,olval,newval)g1 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_g2", function(convar,olval,newval)g2 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_g3", function(convar,olval,newval)g3 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_g4", function(convar,olval,newval)g4 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_b1", function(convar,olval,newval)b1 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_b2", function(convar,olval,newval)b2 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_b3", function(convar,olval,newval)b3 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_b4", function(convar,olval,newval)b4 = newval end)
+	cvars.AddChangeCallback( "metrostroi_routes_secret", function(convar,olval,newval)Secret = newval end)
 
 	hook.Add( "PopulateToolMenu", "Metrostroi Routes Control Panel", function()
 		spawnmenu.AddToolMenuOption( "Utilities", "Metrostroi", "metrostroi_client_panel_routes", "Маршрутники", "", "", function(panel)
@@ -85,27 +267,39 @@ if CLIENT then
 			panel:TextEntry("Выдать \nмрашрутный лист","metrostroi_routes_setlistnumber")
 			panel:Button("Выдать следующий мрашрутный лист","metrostroi_routes_next")
 			panel:Button("Выдать предыдущий мрашрутный лист","metrostroi_routes_prev")
+			panel:Help("\nЦвета:")
+			panel:Help("Фон таблицы")
+			panel:NumSlider("Красный","metrostroi_routes_r1",0, 255,0)
+			panel:NumSlider("Зеленый","metrostroi_routes_g1",0, 255,0)
+			panel:NumSlider("Синий","metrostroi_routes_b1",0, 255,0)
+			panel:NumSlider("Прозрачность","metrostroi_routes_alph",0, 255,0)
+			panel:Help("\nСетка")
+			panel:NumSlider("Красный","metrostroi_routes_r2",0, 255,0)
+			panel:NumSlider("Зеленый","metrostroi_routes_g2",0, 255,0)
+			panel:NumSlider("Синий","metrostroi_routes_b2",0, 255,0)
+			panel:NumSlider("Прозрачность","metrostroi_routes_alph2",0, 255,0)
+			panel:Help("\nТекст")
+			panel:NumSlider("Красный","metrostroi_routes_r3",0, 255,0)
+			panel:NumSlider("Зеленый","metrostroi_routes_g3",0, 255,0)
+			panel:NumSlider("Синий","metrostroi_routes_b3",0, 255,0)
+			panel:NumSlider("Прозрачность","metrostroi_routes_alph3",0, 255,0)
+			panel:Help('\nОбводка текста')
+			panel:NumSlider("Красный","metrostroi_routes_r4",0, 255,0)
+			panel:NumSlider("Зеленый","metrostroi_routes_g4",0, 255,0)
+			panel:NumSlider("Синий","metrostroi_routes_b4",0, 255,0)
+			panel:NumSlider("Прозрачность","metrostroi_routes_alph4",0, 255,0)
+			panel:NumSlider("Секрет","metrostroi_routes_secret",0, 5)
 		end)
 	end)
 
-	local h1,w1
-	local function GetW(RowNumber,ColNumber)
-		local w = 0
-		for i = 1,ColNumber do 
-			if i == ColNumber then continue end
-			w1,h1 = surface.GetTextSize(MaxCols[i] or "")
-			w = w1 + w + 10
-		end
-		return w
-	end
-
+	local LastColorUpdate = CurTime()
 	hook.Add( "HUDPaint", "Draw Route List", function()
 		if not RouteList then return end
+		--[[--старый вариант отрисовки
 		yPos = ypos:GetInt()
 		xPos = xpos:GetInt()
 
 		surface.SetFont(font)
-		local PrevW
 		for RowNumber,Col in pairs(RouteList) do
 			for ColNumber,val in pairs(Col) do
 				if val == "-" then continue end
@@ -115,10 +309,47 @@ if CLIENT then
 				local w,h = surface.GetTextSize(val)
 				draw.RoundedBox( 10, XPos-2, YPos-1,w+5,h, Color(0, 0, 0, 150))
 				draw.SimpleText( val, font, XPos, YPos, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT,TEXT_ALIGN_TOP )
-				PrevW = surface.GetTextSize(val)
+			end
+		end
+		]]
+		local Secret1 = tonumber(Secret)
+		if Secret1 and Secret1 > 0 then
+			if CurTime() - LastColorUpdate > Secret1 then
+				LastColorUpdate = CurTime()
+				Alph:SetInt(math.random(0,255))
+				Alph2:SetInt(math.random(0,255))
+				Alph3:SetInt(math.random(0,255))
+				Alph4:SetInt(math.random(0,255))
+
+				r1ConVar:SetInt(math.random(0,255))
+				r2ConVar:SetInt(math.random(0,255))
+				r3ConVar:SetInt(math.random(0,255))
+				r4ConVar:SetInt(math.random(0,255))
+
+				g1ConVar:SetInt(math.random(0,255))
+				g2ConVar:SetInt(math.random(0,255))
+				g3ConVar:SetInt(math.random(0,255))
+				g4ConVar:SetInt(math.random(0,255))
+
+				b1ConVar:SetInt(math.random(0,255))
+				b2ConVar:SetInt(math.random(0,255))
+				b3ConVar:SetInt(math.random(0,255))
+				b4ConVar:SetInt(math.random(0,255))
 			end
 		end
 
+		--1 - фон, 2 - сетка, 3 - текст, 4 - фон текста
+		surface.SetFont(font)
+		draw.RoundedBox( 0, xPos, yPos,Maxs[3] or 0,Maxs[4] or 0, Color(r1,g1,b1,alph))
+		surface.SetDrawColor(Color(r2,g2,b2,alph2))
+		for _,line in pairs(Lines) do
+			surface.DrawLine( line.Start.x, line.Start.y, line.End.x , line.End.y )
+		end
+		for k,str in pairs(StringsPositions) do
+			--print(xPos, yPos,StringsPositions[#StringsPositions].wB,StringsPositions[#StringsPositions].hB)
+			--draw.SimpleText( str.val, font, str.xpos, str.ypos, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT,TEXT_ALIGN_TOP )
+			draw.SimpleTextOutlined( str.val, font, str.xpos, str.ypos, Color( r3, g3, b3, alph3), TEXT_ALIGN_LEFT,TEXT_ALIGN_TOP,1,Color(r4,g4,b4,alph4) )
+		end
 	end)
 end
 
@@ -207,8 +438,8 @@ local function CreateRoutesTbl()
 	end
 	
 	local Lists = {}
-	for RouteNumber,Row in ipairs(Matrix) do
-		for RowNumber,vals in ipairs(Row) do
+	for RouteNumber,Row in pairs(Matrix) do
+		for RowNumber,vals in pairs(Row) do
 			if AllValuesIsEmpty(vals) and Row[RowNumber+1] and AllValuesIsEmpty(Row[RowNumber+1]) then
 				if not Lists[RouteNumber] then Lists[RouteNumber] = {} end
 				local ListsCount = #Lists[RouteNumber]+1
@@ -221,7 +452,7 @@ local function CreateRoutesTbl()
 	end
 	--PrintTable(Matrix)
 
-	for RouteNumber,matrix in ipairs(Matrix) do
+	for RouteNumber,matrix in pairs(Matrix) do
 		if not istable(matrix) then continue end
 		if not Lists[RouteNumber] then continue end
 		if not Metrostroi.MetrostroiRoutes.RoutesTbl[RouteNumber] then Metrostroi.MetrostroiRoutes.RoutesTbl[RouteNumber] = {} end
